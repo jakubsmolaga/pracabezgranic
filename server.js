e1ea957e13724d209c9f9cc30597dee8b0720a39
smolagakuba <smolagakuba@gmail.com>
2019-04-11 21:47:18 +0200

    przepraszanie bulwy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
-  res.send("BULWIE WALĄ GIRY");
+  res.send("<h1 style='color:pink;'>ŻARTOWAŁEM MIKOŁAJ NIE OBRAŻAJ SIĘ</h1>");
 });
 
 app.listen(PORT, ()=>console.log('server is running!'));
