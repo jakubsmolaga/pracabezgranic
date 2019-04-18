a159adccbe21c646d0b55329f7074052a2c7d232
smolagakuba <smolagakuba@gmail.com>
2019-04-18 16:47:45 +0200

    new landing page

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const staticDirectoryPath = path.join(__dirname, '../static')
+
+let app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(staticDirectoryPath));
+
+app.get('/', (req, res) => res.render('index') );
+
+app.listen(PORT, ()=>console.log(`server running on port ${PORT}`));
--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,3 @@
-let rot=0;
 window.onload = ()=>{
-  setInterval(()=>{
-    document.getElementById('starybulwy').style=`transform: rotate(${rot}deg);`;
-    rot+= 20;
-    if(rot >= 360)rot-=360;
-  }, 1000/20);
+  console.log("page loaded!");
 }
