fdea573beb7b4a7f83c6d9dbf7a6464e9d01c94d
smolagakuba <smolagakuba@gmail.com>
2019-04-26 02:06:53 +0200

    still working on login system

--- server/authentication.js
+++ server/authentication.js
@@ -1,39 +1,38 @@
+const session = require('express-session');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
+const redisClient = redis.createClient(process.env.REDIS_URL);
 
-// let setupCookies = (app) => {
-//   const store = new RedisStore({
-//     host: '127', port: process.env.PORT || 3000, pass: 'secret'
-//   });
-//   app.use(session({
-//     store,
-//     name: 'pracabezgranic_sid',
-//     saveUninitalized: false,
-//     resave: false,
-//     secret: 'purple haze all in my brain',
-//     cookie: {
-//       maxAge: 1000*60*60*2,
-//       sameSite: true,
-//       secure: process.NODE_ENV === 'production'
-//     }
-//   }));
-// };
+let setup = (app) => {
+  app.use(session({
+    secret: 'mamma mia!',
+    store: new RedisStore({host: 'localhost', port: 6379, client: redisClient, ttl: 3600}),
+    saveUninitalized: false,
+    resave: false
+  }));
+};
 
-let register = async (data, db) => {
-  if(!(data.username&&data.email&&
-      data.phonenumber&&data.password)) return 'Nie udało się utworzyć konta.';
+let register = async (req, db) => {
+  let data = req.body;
+  if(!data.username || !data.email || !data.phonenumber || !data.password)
+    return 'Nie udało się utworzyć konta.';
   let user = await db.collection('users').findOne({email: data.email});
-  if (user != null) return 'użytkownik o podanym adresie email już istnieje';
-
+  if (user != null) return 'Użytkownik o podanym adresie email już istnieje';
   db.collection('users').insertOne({
     username: data.username,
     email: data.email,
     phonenumber: data.phonenumber,
     password: data.password
   });
+  req.session.username = data.username;
 };
-let login = async (data, db) => {
+
+let login = async (req, db) => {
+  let data = req.body;
   if(!data.email || !data.password) return 'Nie udało się zalogować.';
   let user = await db.collection('users').findOne({email: data.email});
   if(user == null || data.password != user.password) return 'Nie udało się zalogować.';
+  req.session.username = user.username;
 }
 
-module.exports = {register, login};
+module.exports = {setup, register, login};
--- server/server.js
+++ server/server.js
@@ -8,18 +8,8 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb+srv://smolagakuba:SuperHasloJakub0512@pracabezgranic-d69o4.mongodb.net/test?retryWrites=true';
 const clusterName = 'PracaBezGranic';
 
-const session = require('express-session');
-const redis = require('redis');
-const RedisStore = require('connect-redis')(session);
-const redisClient = redis.createClient(process.env.REDIS_URL);
-
 let app = express();
-app.use(session({
-  secret: 'mamma mia!',
-  store: new RedisStore({host: 'localhost', port: 6379, client: redisClient, ttl: 2600}),
-  saveUninitalized: false,
-  resave: false
-}));
+authentication.setup(app);
 app.set('view engine', 'hbs');
 app.use(express.static(staticDirectoryPath));
 app.use(express.urlencoded());
@@ -30,33 +20,27 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
 
   app.get('/',          (req, res) => res.render('index'));
   app.get('/register',  (req, res) => {
-    if(req.session.email) res.redirect('/dashboard');
+    if(req.session.username) res.redirect('/dashboard');
     else res.render('register')
   });
   app.get('/login',     (req, res) => {
-    if(req.session.email) res.redirect('/dashboard');
+    if(req.session.username) res.redirect('/dashboard');
     else res.render('login');
   });
   app.get('/dashboard', (req, res) => {
-    if(req.session.email) res.render('dashboard');
+    if(req.session.username) res.render('dashboard', {username: req.session.username});
     else res.redirect('/login');
   });
   app.post('/register', (req, res) => {
-    authentication.register(req.body, db).then((error) => {
+    authentication.register(req, db).then((error) => {
       if(error) res.render('register', {error});
-      else{
-        req.session.email = req.body.email;
-        res.redirect('/dashboard');
-      }
+      else res.redirect('/dashboard');
     });
   });
   app.post('/login', (req, res) => {
-    authentication.login(req.body, db).then((error) => {
+    authentication.login(req, db).then((error) => {
       if(error) res.render('login', {error});
-      else{
-        req.session.email = req.body.email;
-        res.redirect('/dashboard');
-      }
+      else res.redirect('/dashboard');
     });
   });
   app.post('/logout', (req, res) => {
