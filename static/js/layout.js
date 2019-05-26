997d0fa7eee79f6eb7db6f895a01ad212f0be55c
smolagakuba <smolagakuba@gmail.com>
2019-05-26 16:00:40 +0200

    zachowywanie ustawien kontrastu i czcionki

--- static/js/layout.js
+++ static/js/layout.js
@@ -8,91 +8,107 @@ x.onclick = function(event) {
 }
 
 <!-- ROZMIAR CZCIONKI -->
+var fontSize = getCookieValue('fontSize');
+if(fontSize == 'medium') FontMedium();
+if(fontSize == 'large')  FontLarge();
+
 function FontDefault() {
   document.getElementById('html').style.fontSize = '1vw';
+  setCookie('fontSize', 'default');
 }
+
 function FontMedium() {
   document.getElementById('html').style.fontSize = '1.2vw';
+  setCookie('fontSize', 'medium');
 }
+
 function FontLarge() {
   document.getElementById('html').style.fontSize = '1.4vw';
+  setCookie('fontSize', 'large');
 }
 
 <!-- KONTRAST -->
-function ContrastDefault(){
-let elementy = document.getElementsByClassName("contrast");
-for(e of elementy) e.style.color = "";
-for(e of elementy) e.style.background = "";
-elementy = document.getElementsByClassName("X");
-for(e of elementy) e.style.background = "";
-elementy = document.getElementsByClassName("contrast-container");
-for(e of elementy) e.style.boxShadow = "";
-for(e of elementy) e.style.background = "";
-elementy = document.getElementsByClassName("contrast-header");
-for(e of elementy) e.style.background = "";
-for(e of elementy) e.style.boxShadow = "";
-elementy = document.getElementsByClassName("contrast-button");
-for(e of elementy) e.style.color = "";
-for(e of elementy) e.style.borderColor = "";
-for(e of elementy) e.style.background = "";
-elementy = document.getElementsByClassName("contrast-input");
-for(e of elementy) e.style.color = "";
-for(e of elementy) e.style.borderColor = "";
-for(e of elementy) e.style.background = "";
-elementy = document.getElementsByClassName("contrast-reverse");
-for(e of elementy) e.style.color = "";
-elementy = document.getElementsByClassName("contrast-icon");
-for(e of elementy) e.style.color = "";
+let contrastMode = getCookieValue('contrastMode');
+if(contrastMode == 'black')  ContrastBlack();
+if(contrastMode == 'yellow') ContrastYellow();
+
+function ContrastDefault() {
+  let elementy = document.getElementsByClassName("contrast");
+  for (e of elementy) e.style.color = "";
+  for (e of elementy) e.style.background = "";
+  elementy = document.getElementsByClassName("X");
+  for (e of elementy) e.style.background = "";
+  elementy = document.getElementsByClassName("contrast-container");
+  for (e of elementy) e.style.boxShadow = "";
+  for (e of elementy) e.style.background = "";
+  elementy = document.getElementsByClassName("contrast-header");
+  for (e of elementy) e.style.background = "";
+  for (e of elementy) e.style.boxShadow = "";
+  elementy = document.getElementsByClassName("contrast-button");
+  for (e of elementy) e.style.color = "";
+  for (e of elementy) e.style.borderColor = "";
+  for (e of elementy) e.style.background = "";
+  elementy = document.getElementsByClassName("contrast-input");
+  for (e of elementy) e.style.color = "";
+  for (e of elementy) e.style.borderColor = "";
+  for (e of elementy) e.style.background = "";
+  elementy = document.getElementsByClassName("contrast-reverse");
+  for (e of elementy) e.style.color = "";
+  elementy = document.getElementsByClassName("contrast-icon");
+  for (e of elementy) e.style.color = "";
+  setCookie('contrastMode', 'default');
 }
 
-function ContrastBlack(){
-let elementy = document.getElementsByClassName("contrast");
-for(e of elementy) e.style.color = "white";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("X");
-for(e of elementy) e.style.background = "white";
-elementy = document.getElementsByClassName("contrast-container");
-for(e of elementy) e.style.boxShadow = "none";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("contrast-header");
-for(e of elementy) e.style.background = "black";
-for(e of elementy) e.style.boxShadow = "0vw 0.1vw 0vw white";
-elementy = document.getElementsByClassName("contrast-button");
-for(e of elementy) e.style.color = "white";
-for(e of elementy) e.style.borderColor = "white";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("contrast-input");
-for(e of elementy) e.style.color = "white";
-for(e of elementy) e.style.borderColor = "white";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("contrast-icon");
-for(e of elementy) e.style.color = "white";
-elementy = document.getElementsByClassName("contrast-reverse");
-for(e of elementy) e.style.color = "white";
+function ContrastBlack() {
+  let elementy = document.getElementsByClassName("contrast");
+  for (e of elementy) e.style.color = "white";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("X");
+  for (e of elementy) e.style.background = "white";
+  elementy = document.getElementsByClassName("contrast-container");
+  for (e of elementy) e.style.boxShadow = "none";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("contrast-header");
+  for (e of elementy) e.style.background = "black";
+  for (e of elementy) e.style.boxShadow = "0vw 0.1vw 0vw white";
+  elementy = document.getElementsByClassName("contrast-button");
+  for (e of elementy) e.style.color = "white";
+  for (e of elementy) e.style.borderColor = "white";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("contrast-input");
+  for (e of elementy) e.style.color = "white";
+  for (e of elementy) e.style.borderColor = "white";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("contrast-icon");
+  for (e of elementy) e.style.color = "white";
+  elementy = document.getElementsByClassName("contrast-reverse");
+  for (e of elementy) e.style.color = "white";
+  setCookie('contrastMode', 'black');
 }
 
-function ContrastYellow(){
-let elementy = document.getElementsByClassName("contrast");
-for(e of elementy) e.style.color = "yellow";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("X");
-for(e of elementy) e.style.background = "yellow";
-elementy = document.getElementsByClassName("contrast-container");
-for(e of elementy) e.style.boxShadow = "none";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("contrast-header");
-for(e of elementy) e.style.background = "black";
-for(e of elementy) e.style.boxShadow = "0vw 0.1vw 0vw yellow";
-elementy = document.getElementsByClassName("contrast-button");
-for(e of elementy) e.style.color = "yellow";
-for(e of elementy) e.style.borderColor = "yellow";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("contrast-input");
-for(e of elementy) e.style.color = "yellow";
-for(e of elementy) e.style.borderColor = "yellow";
-for(e of elementy) e.style.background = "black";
-elementy = document.getElementsByClassName("contrast-icon");
-for(e of elementy) e.style.color = "yellow";
-elementy = document.getElementsByClassName("contrast-reverse");
-for(e of elementy) e.style.color = "white";
+function ContrastYellow() {
+  let elementy = document.getElementsByClassName("contrast");
+  for (e of elementy) e.style.color = "yellow";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("X");
+  for (e of elementy) e.style.background = "yellow";
+  elementy = document.getElementsByClassName("contrast-container");
+  for (e of elementy) e.style.boxShadow = "none";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("contrast-header");
+  for (e of elementy) e.style.background = "black";
+  for (e of elementy) e.style.boxShadow = "0vw 0.1vw 0vw yellow";
+  elementy = document.getElementsByClassName("contrast-button");
+  for (e of elementy) e.style.color = "yellow";
+  for (e of elementy) e.style.borderColor = "yellow";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("contrast-input");
+  for (e of elementy) e.style.color = "yellow";
+  for (e of elementy) e.style.borderColor = "yellow";
+  for (e of elementy) e.style.background = "black";
+  elementy = document.getElementsByClassName("contrast-icon");
+  for (e of elementy) e.style.color = "yellow";
+  elementy = document.getElementsByClassName("contrast-reverse");
+  for (e of elementy) e.style.color = "white";
+  setCookie('contrastMode', 'yellow');
 }
---
+++ static/js/utils.js
@@ -0,0 +1,23 @@
+function setCookie(key, value){
+  key = escape(key);
+  value = escape(value)
+  var cookie = key + '=' + value;
+  document.cookie = cookie;
+}
+
+function getCookies(){
+  var cookies = document.cookie.split(';');
+  var result = {};
+  for(c of cookies){
+    c = c.split('=');
+    var key = c[0].trim();
+    var value = c[1].trim();
+    result[key] = value;
+  }
+  return result;
+}
+
+function getCookieValue(key){
+  var cookies = getCookies();
+  return cookies[key];
+}
